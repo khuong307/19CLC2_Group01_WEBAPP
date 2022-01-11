ca7d2c205a1712c779897860e2c036a4920b1163
khuong307 <85556946+khuong307@users.noreply.github.com>
2022-01-11 22:26:50 +0700

    Fix Captcha + TinyDes + Egister

--- 19CLC2_Group01_WebApp/routes/account.route.js
+++ 19CLC2_Group01_WebApp/routes/account.route.js
@@ -25,7 +25,8 @@ router.post('/register', async function(req, res){
 
     let newUserID = 0;
     const id_present = await accountModel.countUser();
-    let number = parseInt(id_present[0].NumberOfUser) + 1;
+    const stt = id_present.UserID.slice(1, 4);
+    let number = parseInt(stt) + 1;
     if ( number>= 100){
         newUserID = "U" + number
     }
